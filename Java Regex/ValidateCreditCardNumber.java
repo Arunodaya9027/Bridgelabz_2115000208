b5857af4d7b778d2382472cafab80b1513a50867
Arunodaya9027 <arunodaya9027p.sg@gmail.com>
2025-02-22 23:24:18 +0530

    [Arunodaya Pratap Singh] Add. Java Regex All Problems Solution

--- Java Regex/ValidateCreditCardNumber.java
+++ Java Regex/ValidateCreditCardNumber.java
@@ -3,16 +3,18 @@ import java.util.regex.*;
 
 public class ValidateCreditCardNumber {
     public static void main(String[] args) {
-        String creditCardNumber;
-        String regex = "^(?:(?<visa>4[0-9]{12}(?:[0-9]{3})?)|(?<mastercard>5[1-5][0-9]{14})|(?<discover>6(?:011|5[0-9]{2})[0-9]{12})|(?<amex>3[47][0-9]{13})|(?<diners>3(?:0[0-5]|[68][0-9])?[0-9]{11})|(?<jcb>(?:2131|1800|35[0-9]{3})[0-9]{11}))$";
-        Pattern pattern = Pattern.compile(regex);
+        String visaRegex = "^4\\d{15}$";
+        String masterCardRegex = "^5\\d{15}$";
+        Pattern visaPattern = Pattern.compile(visaRegex);
+        Pattern masterCardPattern = Pattern.compile(masterCardRegex);
 
         Scanner input = new Scanner(System.in);
         System.out.print("Enter credit card number: ");
-        creditCardNumber = input.nextLine();
+        String creditCardNumber = input.nextLine();
         
-        Matcher matcher = pattern.matcher(creditCardNumber);
-        System.out.printf("Credit card number is %s.", (matcher.matches() ? "valid" : "invalid"));
+        Matcher visaMatcher = visaPattern.matcher(creditCardNumber);
+        Matcher masterCardMatcher = masterCardPattern.matcher(creditCardNumber);
+        System.out.printf("Credit card number is %s.", (visaMatcher.matches() || masterCardMatcher.matches() ? "valid" : "invalid"));
         input.close();
     }
 }
